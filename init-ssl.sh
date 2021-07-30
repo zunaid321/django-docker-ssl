f55c5cde00027070da12d854025fc7b8f4d5e63f
Zunaid Amin Enan <zunaid321@gmail.com>
2021-07-31 05:46:53 +0600

    Update init-ssl.sh

--- init-ssl.sh
+++ init-ssl.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(site.ru www.site.ru)
+domains=(hdm.tnz.mybluehost.me www.hdm.tnz.mybluehost.me)
 rsa_key_size=4096
 data_path="./nginx/certbot"
-email="info@site.ru" # Adding a valid address is strongly recommended
+email="shawn.novel@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
